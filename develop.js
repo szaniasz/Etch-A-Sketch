4d4b0560f1a617a19137dacd970579e2341fb36a
szaniasz <szewczyk.ann@gmail.com>
2023-03-06 11:34:08 +0100

    Add button to customize grid

--- develop.js
+++ develop.js
@@ -1,4 +1,30 @@
 for(let i = 0; i < 16; i++) {
     const square = document.createElement('div');
+    square.setAttribute('id', 'square');
     document.getElementById('container').appendChild(square);
 }
+
+const buttonPopup = document.createElement('button');
+buttonPopup.setAttribute('id', 'popup');
+buttonPopup.textContent = 'Customize'
+document.getElementById('button').appendChild(buttonPopup);
+
+// let userNumber = ''
+buttonPopup.onclick = function buttonChoose(){ 
+        const userNumber = window.prompt("Number","4");
+            if (userNumber >= 100) {
+                window.confirm('Choose number less than 100');
+                buttonChoose();
+            } else {
+                const divs = document.querySelectorAll("#square");
+                divs.forEach(div => div.remove())
+                for(let i = 0; i < (userNumber*userNumber); i++) {
+                    const square = document.createElement('div');
+                    square.setAttribute('id', 'square');
+                    document.getElementById('container').appendChild(square);
+                }
+                //  debugger
+                 document.documentElement.style.setProperty('--user-number', userNumber);
+            }
+    }
+
